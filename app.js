563b06023fcb9eaf38a5f88e032fad4536010916
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2020-12-15 19:53:34 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const canvas=document.getElementById('jsCanvas');
 
-function onMouseMove(event){
-    console.log(event);
+function onMouseMove(event){    
+    const x =event.offsetX;
+    const y =event.offsetY;
+    console.log(x,y);
+
 }
 
+function onMouseDown(event){
+    
+}
 
 if(canvas){
     canvas.addEventListener("mousemove",onMouseMove);
+    canvas.addEventListener("mousedwon",onMouseDown)
 }
